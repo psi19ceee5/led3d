e8aeffa8a1fb6529f04b7d5b4aceace08edffeb6
Evelyn Ruehl <evelyn.ruehl@student.uni-siegen.de>
2022-12-21 18:53:57 +0100

    added exceptions to LED_ON.py

--- raspi/LED_ON.py
+++ raspi/LED_ON.py
@@ -4,8 +4,19 @@ import sys
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 150)
+num_pixels = 150
 
-ledid = sys.argv[1]
+pixels = neopixel.NeoPixel(board.D18, num_pixels)
 
-pixels[ledid] = (0, 0, 255)
+try:
+    ledid = int(sys.argv[1])
+except TypeError:
+    print("Please enter an integer value between 0 and ", num_pixels-1, "!")
+except IndexError:
+    print("Please specify the ID of the LED you want to turn on (between 0 and ", num_pixels -1, ")!")
+
+if ledid < num_pixels and 0 <= ledid:
+    pixels[ledid] = (0, 0, 255)
+    pixels.show()
+else:
+    print("The LED ID needs to be between 0 and ", num_pixels-1, "!")
