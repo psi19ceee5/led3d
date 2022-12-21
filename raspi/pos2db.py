6331b9d165127a337e2ebd1ceb876147f266f579
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-12-21 14:38:27 +0100

    Added script pos2db.py

---
+++ raspi/pos2db.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3                                                     
+
+import sys
+import sqlite3
+from sqlite3 import Error
+
+angle = sys.argv[1]
+posx = sys.argv[2]
+posy = sys.argv[3]
