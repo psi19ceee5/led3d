fea37625fd6d89e57b0c9a449397d68996bb10ff
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-12-21 19:59:22 +0100

    Fixed bug in calib_positions.sh

--- calib_positions.sh
+++ calib_positions.sh
@@ -9,8 +9,9 @@ sleep(5)
 echo "Starting calibration cycle for angle = $ANGLE deg at `date`"
 
 for (( i=0; i<$NLEDs; i++ )); do
-  ssh pokke """ source ${PROJECTPATH}/activate; 
-                sudo ${PROJECTPATH}/raspi/LED_ON.py $i """
+  ssh pokke """ source ${PROJECTPATH}/activate;
+                cd ${PROJECTPATH}/raspi
+                sudo ./LED_ON.py $i """
   VALS=$(python3 take_photo.py -a $ANGLE)
   ssh pokke """ source ${PROJECTPATH}/activate; 
                 ${PROJECTPATH}/testing/accept_args_for_writing.py $VALS; 
