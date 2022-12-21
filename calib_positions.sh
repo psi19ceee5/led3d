11901802a7752a8a03f466d992b7304827823baa
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-12-21 20:35:49 +0100

    Fixed bugs in calib_positions.sh

--- calib_positions.sh
+++ calib_positions.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/bash
 
 PROJECTPATH=/home/pi/projects/led3d
-NLEDs=$(cat ${PROJECTPATH}/src/config.py | grep NLEDs | awk -F'=' '{print $2}')
+NLEDs=$(cat ./src/config.py | grep NLEDs | awk -F'=' '{print $2}' | xargs)
 ANGLE=$1
 
 echo "Calibration cycle will start in 5s ..."
-sleep(5)
+sleep 1
 echo "Starting calibration cycle for angle = $ANGLE deg at `date`"
 
-for (( i=0; i<$NLEDs; i++ )); do
-  ssh pokke """ source ${PROJECTPATH}/activate;
-                cd ${PROJECTPATH}/raspi
-                sudo ./LED_ON.py $i """
+for (( i = 0; i < $NLEDs; i++ )); do
+  ssh pokke " source ${PROJECTPATH}/activate; \
+              cd ${PROJECTPATH}/raspi; \
+              sudo ./LED_ON.py $i "
   VALS=$(python3 take_photo.py -a $ANGLE)
-  ssh pokke """ source ${PROJECTPATH}/activate; 
-                ${PROJECTPATH}/testing/accept_args_for_writing.py $VALS; 
-                sudo ${PROJECTPATH}/raspi/OFF.py """
+  ssh pokke " source ${PROJECTPATH}/activate; \
+              ${PROJECTPATH}/testing/accept_args_for_writing.py $VALS; \
+              sudo ${PROJECTPATH}/raspi/OFF.py "
 done
 
 echo "Finished at `date`"
