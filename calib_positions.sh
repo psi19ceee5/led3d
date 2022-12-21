344af35c2b3c4d795ed044184b493b08a48d7606
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-12-21 16:51:30 +0100

    calib_positions.sh: end turn-on and turn-off instructions to raspi

--- calib_positions.sh
+++ calib_positions.sh
@@ -5,7 +5,8 @@ NLEDs=$(cat config.py | grep NLEDs | awk -F'=' '{print $2}')
 ANGLE=$1
 
 for (( i=0; i<$NLEDs; i++ )); do
+  ssh pokke "source ${PROJECTPATH}/activate; sudo ${PROJECTPATH}/raspi/LED_ON.py $i"
   VALS=$(python3 take_photo.py -a $ANGLE)
   ssh pokke "python3 ${PROJECTPATH}/testing/accept_args_for_writing.py $VALS"
+  ssh pokke "source ${PROJECTPATH}/activate; sudo ${PROJECTPATH}/raspi/OFF.py"
 done
-
