119dcb53048af9e5c30c35ab61794e2b5489ab96
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-12-21 17:48:24 +0100

    Reduced overhead from calib_positions.sh

--- calib_positions.sh
+++ calib_positions.sh
@@ -9,10 +9,12 @@ sleep(5)
 echo "Starting calibration cycle for angle = $ANGLE deg at `date`"
 
 for (( i=0; i<$NLEDs; i++ )); do
-  ssh pokke "source ${PROJECTPATH}/activate; sudo ${PROJECTPATH}/raspi/LED_ON.py $i"
+  ssh pokke """ source ${PROJECTPATH}/activate; 
+                sudo ${PROJECTPATH}/raspi/LED_ON.py $i """
   VALS=$(python3 take_photo.py -a $ANGLE)
-  ssh pokke "python3 ${PROJECTPATH}/testing/accept_args_for_writing.py $VALS"
-  ssh pokke "source ${PROJECTPATH}/activate; sudo ${PROJECTPATH}/raspi/OFF.py"
+  ssh pokke """ source ${PROJECTPATH}/activate; 
+                ${PROJECTPATH}/testing/accept_args_for_writing.py $VALS; 
+                sudo ${PROJECTPATH}/raspi/OFF.py """
 done
 
 echo "Finished at `date`"
