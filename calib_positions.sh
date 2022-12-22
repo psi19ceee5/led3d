79ea5d80a09c5909d1a5af9ef7dddb6b0c5078d3
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-12-22 01:48:26 +0100

    calib_positions.sh: cut images below 150 brightness + point to pos2db.py instead of test script

--- calib_positions.sh
+++ calib_positions.sh
@@ -4,18 +4,26 @@ PROJECTPATH=/home/pi/projects/led3d
 NLEDs=$(cat ./src/config.py | grep NLEDs | awk -F'=' '{print $2}' | xargs)
 ANGLE=$1
 
-echo "Calibration cycle will start in 5s ..."
-sleep 1
+source ./activate
+
+echo "Calibration cycle will start in 10s ..."
+sleep 10
 echo "Starting calibration cycle for angle = $ANGLE deg at `date`"
 
 for (( i = 0; i < $NLEDs; i++ )); do
   ssh pokke " source ${PROJECTPATH}/activate; \
               cd ${PROJECTPATH}/raspi; \
               sudo ./LED_ON.py $i "
-  VALS=$(python3 take_photo.py -a $ANGLE)
-  ssh pokke " source ${PROJECTPATH}/activate; \
-              ${PROJECTPATH}/testing/accept_args_for_writing.py $VALS; \
-              sudo ${PROJECTPATH}/raspi/OFF.py "
+  VALS=$(python3 take_photo.py -a $ANGLE -n $i)
+  if (( $(echo $VALS | awk '{print $4}') > 150 )); then
+    ssh pokke " source ${PROJECTPATH}/activate; \
+                ${PROJECTPATH}/raspi/pos2db.py $VALS; \
+                sudo ${PROJECTPATH}/raspi/OFF.py "
+  else
+    ssh pokke " source ${PROJECTPATH}/activate; \
+                sudo ${PROJECTPATH}/raspi/OFF.py "
+  fi
+  
 done
 
 echo "Finished at `date`"
