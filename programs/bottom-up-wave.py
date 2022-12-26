4a2366ae30ad1a736472bc6c02387f3b6dc98f0a
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-12-26 18:53:05 +0100

    Added class ledchain

--- programs/bottom-up-wave.py
+++ programs/bottom-up-wave.py
@@ -10,12 +10,20 @@ import src.dbio as db
 import src.led as led
 
 class led(led.proto_led) :
+    def __init__(self, id, pos=(0, 0, 0), col=(0, 0, 0), ledchain) :
+        super().__init__(id, pos=pos, col=col)
+        self.ledchain = ledchain
+        
     def commit(self) :
         id = self.led_id
         r = round(255*self.r)
         g = round(255*self.g)
         b = round(255*self.b)
-        print(r, g, b)
+        
+        self.ledchain.pixels[id] = (r, g, b)
+                
+class ledchain :
+    def __init__(self, brightness=0.2, ORDER=neopixel.RGB) :
         
         # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
         # NeoPixels must be connected to D10, D12, D18 or D21 to work.
@@ -28,32 +36,32 @@ class led(led.proto_led) :
         # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
         ORDER = neopixel.RGB
         
-        pixels = neopixel.NeoPixel(
-            pixel_pin, num_pixels#, brightness=0.2, auto_write=False, pixel_order=ORDER
+        self.pixels = neopixel.NeoPixel(
+            pixel_pin, num_pixels, brightness=brightness, auto_write=False, pixel_order=ORDER
         )
-        
-        pixels[id] = (r, g, b)
-        pixels.show()
-        
-        
+                
+    def commit(self) :
+        self.pixels.show()
 
 if __name__ == "__main__" :
     
     conn = db.create_connection("../db/calibinfo.sqlite")
 
+    chain = ledchain()
     leds = []
     for id in range(cfg.NLEDs) :
         try :
             (x, y, z) = db.read_led(conn, id)
-            leds.append(led(id, (x, y, z), (1, 1, 1)))
+            leds.append(led(id, (x, y, z), (1, 1, 1), chain))
         except Exception :
             print("[ERROR]: LED", id, " not in database.")
     
-    fps = 25
+    fps = 10.
     phase_r = 0
     phase_g = 60*math.pi/180.
     phase_b = 120*math.pi/180.
     t0 = time.time()
+    dt = 0
     while True :
         t = time.time() - t0
         for led in leds :
@@ -62,3 +70,6 @@ if __name__ == "__main__" :
             b = math.sin(0.5*math.pi*t - (math.pi/0.25)*led.z + phase_b)**2
             led.set_rgb(r, g, b)
             led.commit()
+        chain.commit()
+        dt = time.time() - t0 - t
+        time.sleep(1./fps - dt if dt < 1./fps else 0)
