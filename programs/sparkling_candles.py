afd09cac3e3418c6be304d49f48535981dd1f851
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2023-01-01 15:59:19 +0100

    Implemented shooting star

--- programs/sparkling_candles.py
+++ programs/sparkling_candles.py
@@ -39,9 +39,16 @@ class program(prg.tmpprg) :
         self.candle_brightness = np.array([1]*len(self.candles))
         self.leds_checked = []
         self.t0 = 0
-        self.shootinStarProb = 1.
-        self.shootingStarVel = 1.
+
         self.hasShootingStar = False
+        self.ssProb = 1.
+        self.ssVel = 1.
+        self.ssZenith = 0
+        self.ssAzimuth = 0
+        self.ssStart = np.array([0, 0, 0])
+        self.ssPos = np.array([0, 0, 0])
+        self.ssDir = np.array([0, 0, -1])
+        self.ssRadius = 0.10
         
     def brightness_fkt(self, dt, b0) :
         b0 *= 1000
@@ -53,25 +60,43 @@ class program(prg.tmpprg) :
         bright = bright * (bright > 0).astype(int)
         bright /= 1000
         return bright
-
         
     def set_state(self, t) :
+        dt = 0
         if self.t0 > 0 :
             dt = t - self.t0
             self.candle_brightness = self.brightness_fkt(dt, self.candle_brightness)
         self.t0 = t
         
-        # if no shooting star exists: roll dice
-        if not self.HasShootingStar :
-            if self.shootingStarProb*dt > rnd.random() :
+        if self.hasShootingStar :
+            # 5. update position of star according to vec_pos1 = vec_pos0 + vec_v*dt
+            self.ssPos += self.ssVel*dt*self.ssDir
+            if self.ssPos[2] < 0 :
+                self.hasShootingStar = False
+        else :
+            # if no shooting star exists: roll dice
+            if self.ssProb*dt > rnd.random() :
                 self.hasShootingStar = True
                 # find trajectory and initial state of shooting star
                 # 1. random pivot point within tree-cube
+                #pivot = np.array([rnd.uniform(cfg.xl, cfg.xh), rnd.uniform(cfg.yl, cfg.yh), rnd.uniform(cfg.zl, cfg.zh)])
+                pivot = np.array([0, 0, cfg.zh - 0.1])
                 # 2. random zenith angle between 0 and 60 deg and random azimuth angle between 0 and 360 deg
+                self.ssZenith = rnd.uniform(30*ut.deg2rad, 45*ut.deg2rad)
+                self.ssAzimuth = rnd.uniform(0, 360*ut.deg2rad)
                 # 3. parameterize curve: starting at top-intersection and ending with ground-intersection:
                 #    at t0, star starts at height, propagages with v
+                hyp = math.tan(self.ssZenith)*(cfg.zh - pivot[2])
+                xstart = pivot[0] + math.cos(self.ssAzimuth)*hyp
+                ystart = pivot[1] + math.sin(self.ssAzimuth)*hyp
+                zstart = cfg.zh
                 # 4. write curve-parameters and current position of shooting star to member variables
-                # 5. update position of star according to pos1 = pos0 + v*dt                                
+                self.ssStart = np.array([xstart, ystart, zstart])
+                self.ssPos = self.ssStart
+                self.ssDir = (pivot - self.ssStart)
+                self.ssDir /= np.linalg.norm(self.ssDir)
+
+        print(self.ssPos)                
         
                 
     def fkt(self) :
@@ -88,7 +113,6 @@ class program(prg.tmpprg) :
                     iscandle = True
                     break
                 
-                                
         if iscandle :
             self.r, self.g, self.b = (x*bright for x in self.candle_col)
         else :
@@ -98,4 +122,7 @@ class program(prg.tmpprg) :
             self.leds_checked.append(self.id)
             
         # check if led is close to shooting star and override col = (1, 1, 1)
+        dist_2_ss = np.linalg.norm(np.array(pos) - self.ssPos)
+        if self.hasShootingStar and dist_2_ss < self.ssRadius :
+            self.r, self.g, self.b = (1, 1, 1)
 
